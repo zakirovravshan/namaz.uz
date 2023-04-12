be4c6a030fe5782fe5fe227e8d32010fd159ad2f
zakirovravshan <zakirovrovshan@gmail.com>
2023-04-12 16:43:50 +0500

    add design

--- src/pages/Main.js/Main.jsx
+++ src/pages/Main.js/Main.jsx
@@ -27,10 +27,10 @@ const Main = () => {
 	useEffect(() => {
         getData();
 		updateCurrentDateTime();
-		const intervalId = setInterval(updateCurrentDateTime, 1000);
-		clearInterval(intervalId);
+		
 		
 	}, []);
+	setInterval(updateCurrentDateTime, 1000);
 
 	return (
 		<div className='wrapper'>
