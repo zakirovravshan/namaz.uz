3c959ce0c601d9435e7261c33d5aa738ec2bc4d8
zakirovravshan <zakirovrovshan@gmail.com>
2023-04-13 16:56:47 +0500

    add tasbeeh

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import './Settings.css';
 import { useState } from 'react';
+import redo from '../../assets/icons/redo.png';
 
 const Settings = () => {
 	var [counter, setCounter] = useState(0);
 
 	var handleCount = () => {
 		setCounter(counter + 1);
-		if (counter === 33) {
-			setCounter(0);
-		}
+		// if (counter === 33) {
+		// 	setCounter(0);
+		// }
 	};
 
 	return (
@@ -18,6 +19,9 @@ const Settings = () => {
 				<p className='tasbeh__display'>{counter}</p>
 				<button className='tasbeh__button' onClick={() => handleCount()}>
 					Sanoq uchun shu yerga bosing
+				</button>
+				<button onClick={() => setCounter(0)} className='reset__button'>
+					<img src={redo} alt='redo' width={'40px'} />
 				</button>
 			</div>
 		</div>
