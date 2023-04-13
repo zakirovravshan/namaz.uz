9f7eefdd3755149647d89819f83f25b996e58ba9
zakirovravshan <zakirovrovshan@gmail.com>
2023-04-13 11:22:21 +0500

    add bar style

--- src/components/Bar/Bar.jsx
+++ src/components/Bar/Bar.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import './Bar.css';
 import { Link } from 'react-router-dom';
+import tasbeh from '../../assets/icons/tasbeh.png';
 
 export const Bar = () => {
 	return (
 		<div className='bar'>
-			<ul>
-				<li>
-					<Link to="/">  </Link>
+			<ul className='bar__list'>
+				<li className='bar__item'>
+					<Link to='/'>
+						{' '}
+						<img src={tasbeh} alt='tasbeh' width={'50px'} />{' '}
+					</Link>
+				</li>
+				<li className='bar__item'>
+					<Link to='/'>
+						{' '}
+						<img src={tasbeh} alt='tasbeh' width={'50px'} />{' '}
+					</Link>
+				</li>
+				<li className='bar__item'>
+					<Link to='/'>
+						{' '}
+						<img src={tasbeh} alt='tasbeh' width={'50px'} />{' '}
+					</Link>
+				</li>
+				<li className='bar__item'>
+					<Link to='/'>
+						{' '}
+						<img src={tasbeh} alt='tasbeh' width={'50px'} />{' '}
+					</Link>
 				</li>
 			</ul>
 		</div>
