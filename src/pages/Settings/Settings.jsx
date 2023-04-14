511811a3c1e74440fdf6f9137c07245309cfd899
zakirovravshan <zakirovrovshan@gmail.com>
2023-04-14 16:00:46 +0500

    add zikrs

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -8,18 +8,82 @@ import { Button } from '@mui/material';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Slider from 'react-slick';
+import { Toaster, toast } from 'react-hot-toast';
 
 const Settings = () => {
-	const [modal, setModal] = useState(false);
 	var [counter, setCounter] = useState(localStorage.getItem('counter'));
+	var [counter2, setCounter2] = useState(localStorage.getItem('counter2'));
+	var [counter3, setCounter3] = useState(localStorage.getItem('counter3'));
+	var [counter4, setCounter4] = useState(localStorage.getItem('counter4'));
+	var [counter5, setCounter5] = useState(localStorage.getItem('counter5'));
 
-	var handleCount = () => {
+	var handleCount1 = () => {
+		if (counter == 0) {
+			toast.success('Subhanalloh 33ta', {
+				duration: 8000,
+			});
+		}
+		if (counter == 32) {
+			toast.success('Alhamdulillah 33ta ❗️', {
+				duration: 8000,
+			});
+		}
+		if (counter == 65) {
+			toast.success('Allohu Akbar 33ta ❗️', {
+				duration: 8000,
+			});
+		}
 		localStorage.setItem('counter', counter);
-		var number = JSON.parse(localStorage.getItem('counter'));
-		var sum = number + 1;
-		localStorage.setItem('counter', sum);
+		var number1 = JSON.parse(localStorage.getItem('counter'));
+		var sum1 = number1 + 1;
+		localStorage.setItem('counter', sum1);
 		setCounter(localStorage.getItem('counter'));
 	};
+	var handleCount2 = () => {
+		if (counter2 == 99) {
+			toast.success('Ma sha Alloh!');
+		}
+
+		localStorage.setItem('counter2', counter2);
+		var number2 = JSON.parse(localStorage.getItem('counter2'));
+		var sum2 = number2 + 1;
+		localStorage.setItem('counter2', sum2);
+		setCounter2(localStorage.getItem('counter2'));
+	};
+	var handleCount3 = () => {
+		if (counter2 == 99) {
+			toast.success('Ma sha Alloh!');
+		}
+		localStorage.setItem('counter3', counter3);
+		var number3 = JSON.parse(localStorage.getItem('counter3'));
+		var sum3 = number3 + 1;
+		localStorage.setItem('counter3', sum3);
+		setCounter3(localStorage.getItem('counter3'));
+	};
+	var handleCount4 = () => {
+		if (counter2 == 99) {
+			toast.success('Ma sha Alloh!');
+		}
+		localStorage.setItem('counter4', counter4);
+		var number4 = JSON.parse(localStorage.getItem('counter4'));
+		var sum4 = number4 + 1;
+		localStorage.setItem('counter4', sum4);
+		setCounter4(localStorage.getItem('counter4'));
+	};
+	// var handleCount5 = () => {
+	// 	if (counter5 == 32) {
+	// 		toast.success('Ma sha Alloh!');
+	// 	}
+	// 	if (counter5 == 33 || counter5 == 34) {
+	// 		toast.error('33 tadan kopaytirib yubordingiz!');
+	// 	}
+	// 	localStorage.setItem('counter5', counter5);
+	// 	var number5 = JSON.parse(localStorage.getItem('counter5'));
+	// 	var sum5 = number5 + 1;
+	// 	localStorage.setItem('counter5', sum5);
+	// 	setCounter5(localStorage.getItem('counter5'));
+	// };
+
 	var settings = {
 		centerMode: true,
 		centerPadding: '80px',
@@ -57,79 +121,117 @@ const Settings = () => {
 		],
 	};
 	return (
-		<Slider className='big_box' {...settings}>
-			<div className='box'>
-				<div className='wrapper'>
-					<p className='tasbeh__display'>{counter}</p>
-					<motion.button
-						whileHover={{ scale: 1 }}
-						whileTap={{ scale: 0.98 }}
-						transition={{ type: 'spring', stiffness: 400, damping: 17 }}
-						className='tasbeh__button'
-						onClick={() => handleCount()}>
-						<button
-							onClick={() => {
-								setCounter(0);
-								localStorage.setItem('counter', 0);
-							}}
-							className='reset__button'>
-							<img src={redo} alt='redo' width={'20px'} />
-						</button>
-						Sanoq uchun shu yerni bosing
-					</motion.button>
+		<div>
+			<button
+				onClick={() => {
+					setCounter(0);
+					localStorage.setItem('counter', 0);
+					setCounter2(0);
+					localStorage.setItem('counter2', 0);
+					setCounter3(0);
+					localStorage.setItem('counter3', 0);
+					setCounter4(0);
+					localStorage.setItem('counter4', 0);
+				}}
+				className='reset__button'>
+				Reset All
+				<img src={redo} alt='redo' width={'20px'} />
+			</button>
+			<Slider className='big_box' {...settings}>
+				<div className='box'>
+					<div className='wrapper'>
+						<p className='tasbeh__display'>{counter}</p>
+						<motion.button
+							whileHover={{ scale: 1 }}
+							whileTap={{ scale: 0.98 }}
+							transition={{ type: 'spring', stiffness: 400, damping: 17 }}
+							className='tasbeh__button'
+							onClick={() => handleCount1()}>
+							<p className='ar' >سُبْحَانَ اللهِ</p>
+							<p className='tasbeh__33 ar_uz '>Subhanalloh </p>
+
+							<p className='ar'> ٱلْحَمْدُ لِلَّٰهِ</p>
+							<p className='tasbeh__33 ar_uz'>Alhamdulillah </p>
+
+							<p className='ar'> الله أكب</p>
+							<p className='tasbeh__33 ar_uz'>Allahu Akbar </p>
+						</motion.button>
+					</div>
 				</div>
-			</div>
-			<div className='box'>
-				<div className='wrapper'>
-					<p className='tasbeh__display'>{counter}</p>
-					<motion.button
-						whileHover={{ scale: 1 }}
-						whileTap={{ scale: 0.98 }}
-						transition={{ type: 'spring', stiffness: 400, damping: 17 }}
-						className='tasbeh__button'
-						onClick={() => handleCount()}>
-						<button
-							onClick={() => {
-								setCounter(0);
-								localStorage.setItem('counter', 0);
-							}}
-							className='reset__button'>
-							<img src={redo} alt='redo' width={'20px'} />
-						</button>
-						ٱللَّٰهُمَّ صَلِّ عَلَىٰ مُحَمَّدٍ وَعَلَىٰ آلِ مُحَمَّدٍ
-					</motion.button>
-					<button
-						onClick={() => {
-							setCounter(0);
-							localStorage.setItem('counter', 0);
-						}}
-						className='reset__button'>
-						Reset All <img src={redo} alt='redo' width={'20px'} />
-					</button>
+				<div className='box'>
+					<div className='wrapper'>
+						<p className='tasbeh__display'>{counter2}</p>
+						<motion.button
+							whileHover={{ scale: 1 }}
+							whileTap={{ scale: 0.98 }}
+							transition={{ type: 'spring', stiffness: 400, damping: 17 }}
+							className='tasbeh__button'
+							onClick={() => handleCount2()}>
+							<p className='ar_uz'>
+								Allahumma Salli Ala Muhammad Waala Ali Muhammad{' '}
+							</p>
+							<p className='ar'>
+								
+								ٱللَّٰهُمَّ صَلِّ عَلَىٰ مُحَمَّدٍ وَعَلَىٰ آلِ مُحَمَّدٍ
+							</p>
+							<p className='uz'>
+								Allohim! Payg‘ambarimiz Muhammad Mustafoga beadad salot va salom
+								bo‘lsin
+							</p>
+						</motion.button>
+					</div>
 				</div>
-			</div>
-			<div className='box'>
-				<div className='wrapper'>
-					<p className='tasbeh__display'>{counter}</p>
-					<motion.button
-						whileHover={{ scale: 1 }}
-						whileTap={{ scale: 0.98 }}
-						transition={{ type: 'spring', stiffness: 400, damping: 17 }}
-						className='tasbeh__button'
-						onClick={() => handleCount()}>
-						<button
-							onClick={() => {
-								setCounter(0);
-								localStorage.setItem('counter', 0);
-							}}
-							className='reset__button'>
-							<img src={redo} alt='redo' width={'20px'} />
-						</button>
-						Sanoq uchun shu yerni bosing
-					</motion.button>
+				<div className='box'>
+					<div className='wrapper'>
+						<p className='tasbeh__display'>{counter3}</p>
+						<motion.button
+							whileHover={{ scale: 1 }}
+							whileTap={{ scale: 0.98 }}
+							transition={{ type: 'spring', stiffness: 400, damping: 17 }}
+							className='tasbeh__button'
+							onClick={() => handleCount3()}>
+							<p className='ar_uz'>
+								Subhanallohi va bihamdihi subhanallohil aziym{' '}
+							</p>
+							<p className='ar'>
+								
+								سُبْحَانَ اللَّهِ وَبِحَمْدِهِ، سُبْحَانَ اللَّهِ العَظِيم
+							</p>
+							<p className='uz'>
+								Allohni poklab yod etaman va u zotga hamd aytaman
+							</p>
+						</motion.button>
+					</div>
 				</div>
+				<div className='box'>
+					<div className='wrapper'>
+						<p className='tasbeh__display'>{counter4}</p>
+						<motion.button
+							whileHover={{ scale: 1 }}
+							whileTap={{ scale: 0.98 }}
+							transition={{ type: 'spring', stiffness: 400, damping: 17 }}
+							className='tasbeh__button'
+							onClick={() => handleCount4()}>
+							<p className='ar_uz'>
+								La ilaha illa anta subẍanaka inni kuntu mina-ӟ-alimin
+							</p>
+							<p className='ar'>
+								
+								لاَّ إِلَـهَ إِلاَّ أَنتَ سُبْحَـنَكَ إِنِّى كُنتُ مِنَ
+								الظَّـلِمِينَ
+							</p>
+							<p className='uz'>
+								Sendan o‘zga ibodatga loyiq iloh yo‘q, sen har qanday
+								kamchilikdan poksan, albatta, men zolimlardan edim
+							</p>
+						</motion.button>
+					</div>
+				</div>
+			</Slider>
+			<div>
+				<Toaster />
 			</div>
-		</Slider>
+		</div>
 	);
 	// <ReactModal
 	// 	onRequestClose={() => setModal(false)}
