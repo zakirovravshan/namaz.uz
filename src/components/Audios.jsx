4c311b3c7bec9e290c34981831ef3248a2cbba4d
zakirovravshan <zakirovrovshan@gmail.com>
2023-04-20 17:17:53 +0500

    add pause

--- src/components/Audios.jsx
+++ src/components/Audios.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
 import play from '../assets/icons/play.svg';
+import { BsPauseFill } from 'react-icons/bs';
 
 export const Audios = ({ ayahnumber }) => {
 	const [audio, setAudio] = useState('');
+	const [isPlaying, setIsPlaying] = useState(false);
 	const audioRef = useRef(null);
 
 	const getAudio = (number) => {
@@ -12,20 +14,34 @@ export const Audios = ({ ayahnumber }) => {
 		);
 	};
 
+	const handleEnded = () => {
+		setIsPlaying(false); // обновление состояния после окончания воспроизведения
+	};
+
 	useEffect(() => {
 		getAudio(ayahnumber);
 	}, []);
 
 	const handleButtonClick = async () => {
-		audioRef.current.play();
+		if (isPlaying) {
+			audioRef.current.pause(); // приостановка воспроизведения аудио
+			setIsPlaying(false); // обновление состояния
+		} else {
+			audioRef.current.play(); // воспроизведение аудио
+			setIsPlaying(true); // обновление состояния
+		}
 	};
 
 	return (
 		<div>
 			<button onClick={() => handleButtonClick()} className='bar__copy__button'>
-				<img src={play} alt='playbutton' width={'15px'} />
+				{isPlaying ? (
+					<BsPauseFill size={'20px'} color='#03AA77' />
+				) : (
+					<img src={play} alt='playbutton' width={'15px'} />
+				)}
 			</button>
-			<audio ref={audioRef} src={audio} />
+			<audio ref={audioRef} src={audio} onEnded={handleEnded} />
 		</div>
 	);
 };
